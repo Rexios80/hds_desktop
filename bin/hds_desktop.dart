0912511871f047e2e5426bf3d813aeecbf735606
Rexios <rexios@rexios.dev>
2022-09-12 09:58:00 -0400

    ...

--- bin/hds_desktop.dart
+++ bin/hds_desktop.dart
@@ -11,7 +11,12 @@ WebSocketChannel? client;
 void main(List<String> arguments) async {
   final app = Router()
     ..put('/', handleHttpRequest)
-    ..get('/', shelf_ws.webSocketHandler(handleWebSocketConnect));
+    ..get(
+      '/',
+      (request) => shelf_ws.webSocketHandler(
+        (socket) => handleWebSocketConnect(request, socket),
+      )(request),
+    );
 
   final server = await shelf_io.serve(app, '0.0.0.0', 3476);
 
@@ -22,13 +27,20 @@ Future<Response> handleHttpRequest(Request request) async {
   final body = await request.readAsString();
   final json = jsonDecode(body);
   final data = json['data'];
-  print('Received data: $data');
-  client?.sink.add(data);
+  handleData(data);
   return Response.ok(null);
 }
 
-void handleWebSocketConnect(WebSocketChannel socket) {
+void handleWebSocketConnect(Request request, WebSocketChannel socket) {
+  final asdf = request.context['shelf.io.connection_info']!;
+  print('here');
   client = socket;
   print('Client connected');
+  socket.stream.listen((e) => handleData(e));
   socket.sink.done.then((e) => print('Client disconnected'));
+}
+
+void handleData(String data) {
+  print('Received data: $data');
+  client?.sink.add(data);
 }
